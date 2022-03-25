ced15cd55f7d5681b62a5ea8d4010b3c028c29e8
hagooboy <66807067+hagooboy@users.noreply.github.com>
2022-03-24 18:49:20 -0700

    added build js

---
+++ scripts/build.js
@@ -0,0 +1,48 @@
+const fs = require("fs");
+
+// create paths to send to build and send all
+const paths = {
+  build: {
+    root: "./build",
+    assets: "./build/assets",
+  },
+  src: {
+    imageAssets: "./src/assets/img",
+    jsonAssets: "./src/assets/mapData",
+  },
+};
+
+if (!fs.existsSync(paths.build.root)) {
+  fs.mkdirSync(paths.build.root);
+}
+
+if (!fs.existsSync(paths.build.assets)) {
+  const imageAssets = fs.readdirSync(paths.src.imageAssets);
+  const jsonAssets = fs.readdirSync(paths.src.jsonAssets);
+
+  fs.mkdirSync(paths.build.assets);
+
+  for (const asset of imageAssets) {
+    fs.copyFileSync(
+      `${paths.src.imageAssets}/${asset}`,
+      `${paths.build.assets}/${asset}`
+    );
+  }
+
+  for (const asset of jsonAssets) {
+    fs.copyFileSync(
+      `${paths.src.jsonAssets}/${asset}`,
+      `${paths.build.assets}/${asset}`
+    );
+  }
+}
+
+// Call parcel
+require("child_process").spawn(
+  "parcel",
+  ["build","title.html", "index.html", "-p 8000", "--open", "--dist-dir", "build"],
+  {
+    stdio: ["ignore", "inherit", "inherit"],
+    shell: true,
+  }
+);
